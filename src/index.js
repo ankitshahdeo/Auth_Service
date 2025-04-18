53fc647c520464c3baeed2ecf38c519ed99905c9
ankitshahdeo <ankitshahdeo29@gmail.com>
2025-04-18 19:35:15 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser =require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+//const UserRepository = require('./repository/user-repository');
+
 const app = express();
 
 const prepareAndStartService =() => {
@@ -13,8 +15,11 @@ const prepareAndStartService =() => {
     
     app.use('/api',apiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async() => {
         console.log(`Server started on Port: ${PORT}`);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response);
     });
 }
 prepareAndStartService();
